9bf072b6d0528dc97748d6cc511d57355ece0ada
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-04-01 12:53:37 +0530

    removed some files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { AuthContextProvider } from "./Context/AuthContext";
 import { SafeInsureProvider } from "./Context/SafeInsureContext";
 import { AuthProvider } from "@arcana/auth";
