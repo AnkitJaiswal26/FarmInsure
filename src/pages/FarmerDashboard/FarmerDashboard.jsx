c274b99c3554f90327ed35fca2a37df2e50bed65
atharvapatil123 <atharva7275@gmail.com>
2023-04-01 21:12:32 +0530

    UI changes

--- src/pages/FarmerDashboard/FarmerDashboard.jsx
+++ src/pages/FarmerDashboard/FarmerDashboard.jsx
@@ -6,52 +6,63 @@ const FarmerDashboard = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
+  const navigateInsurances = () => {
+    navigate("/insurances");
+  };
+
   return (
     <>
       <div className={styles.dashboardMain}>
-        <h1 className={styles.mainHeading}>Hello Atharva!</h1>
-        <div className={styles.buttons}>
-          <button className={styles.button}>Edit Profile</button>
-          <button className={styles.button}>Change Language</button>
-          <button className={styles.button}>Contact Us</button>
-        </div>
-        <div className={styles.viewFarms}>
-          <div className={styles.farmsHeadBox}>
-            <h2 className={styles.farmsHead}>View My Farms</h2>
-            <button className={styles.button2}>Add Farm</button>
+        <div className={styles.secondContainer}>
+          <h1 className={styles.mainHeading}>Hello Atharva!</h1>
+          <div className={styles.shadow}></div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>Edit Profile</button>
+            <button className={styles.button}>Change Language</button>
+            <button className={styles.button}>Contact Us</button>
           </div>
-          <div className={styles.farmsCards}>
-            <div className={styles.farm}>
-              <div className={styles.headFarm}>fileName</div>
-              <div className={styles.details}>location</div>
-              <div className={styles.details}>landArea</div>
-              <div className={styles.details}>cropType</div>
+          <div className={styles.viewFarms}>
+            <div className={styles.farmsHeadBox}>
+              <h2 className={styles.farmsHead}>View My Farms</h2>
+              <button className={styles.button2}>Add Farm</button>
             </div>
-            <div className={styles.farm}>
-              <div className={styles.headFarm}>fileName</div>
-              <div className={styles.details}>location</div>
-              <div className={styles.details}>landArea</div>
-              <div className={styles.details}>cropType</div>
+            <div className={styles.shadow}></div>
+            <div className={styles.farmsCards}>
+              <div className={styles.farm}>
+                <div className={styles.headFarm}>FileName</div>
+                <div className={styles.details}>Location: <span className={styles.values}>Latur</span></div>
+                <div className={styles.details}>LandArea: <span className={styles.values}>2 acre</span></div>
+                <div className={styles.details}>CropType: <span className={styles.values}>Rabi</span></div>
+              </div>
+              <div className={styles.farm}>
+                <div className={styles.headFarm}>FileName</div>
+                <div className={styles.details}>Location: <span className={styles.values}>Bhusawal</span></div>
+                <div className={styles.details}>LandArea: <span className={styles.values}>1 acre</span></div>
+                <div className={styles.details}>CropType: <span className={styles.values}>Kharif</span></div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.viewInsurances}>
-          <div className={styles.insuranceHeadBox}>
-            <h2 className={styles.insuranceHead}>View My Insurances</h2>
-            <button className={styles.button2}>Explore Insurances</button>
-          </div>
-          <div className={styles.insuranceCards}>
-            <div className={styles.insurance}>
-              {/* uint256 userId;
+          <div className={styles.viewInsurances}>
+            <div className={styles.insuranceHeadBox}>
+              <h2 className={styles.insuranceHead}>View My Insurances</h2>
+              <button onClick={navigateInsurances} className={styles.button2}>
+                Explore Insurances
+              </button>
+            </div>
+            <div className={styles.shadow}></div>
+            <div className={styles.insuranceCards}>
+              <div className={styles.insurance}>
+                {/* uint256 userId;
         string location;
         uint256 landArea;
         string cropType;
         string ipfsHash;
         string fileName; */}
-              <div className={styles.headInsurance}>fileName</div>
-              <div className={styles.details}>period</div>
-              <div className={styles.details}>description</div>
-              <div className={styles.details}>type</div>
+                <div className={styles.headInsurance}>Insurance</div>
+                <div className={styles.details}>Period: <span className={styles.values}>1 year</span></div>
+                <div className={styles.details}>Premium paid : <span className={styles.values}>1000</span></div>
+                <div className={styles.details}>Next Due: <span className={styles.values}>7th April</span></div>
+              </div>
             </div>
           </div>
         </div>
