7dba38621658d0c869eefe8a8184ecf5ace4f85f
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-04-02 02:25:00 +0530

    Connected comp dash

--- src/pages/CompanyDashboard/CompanyDashboard.jsx
+++ src/pages/CompanyDashboard/CompanyDashboard.jsx
@@ -13,202 +13,238 @@ import axios from "../../helpers/axios";
 import { useSafeInsureContext } from "../../Context/SafeInsureContext";
 
 const CompanyDashboard = () => {
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-  const [payout, setPayout] = useState("");
-  const [premium, setPremium] = useState("");
-  const [duration, setDuration] = useState("");
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+	const [payout, setPayout] = useState("");
+	const [premium, setPremium] = useState("");
+	const [duration, setDuration] = useState("");
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const { connectUsingArcana, currentAccount } = useAuth();
 
-  function closeModal() {
-    setModalIsOpen(false);
-  }
+	const {
+		fetchCompanyByAddress,
+		addNewInsuranceType,
+		fetchMyInsList,
+		fetchInsuranceAddress,
+	} = useSafeInsureContext();
 
-  const openModal = async (e) =>{
-    e.preventDefault();
-    setModalIsOpen(true);
-  }
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (duration === "" || premium === "" || payout === "") {
-      toast.error("Enter all details");
-      return;
-    } else {
-      // if (email.slice(-10) === "vjti.ac.in") {
-    //   setModalIsOpen(true);
-    //   console.log(modalIsOpen);
-    //   await axios
-    //     .post("/register", { mobileNo })
-    //     .then((res) => {
-    //       console.log("res", res);
-    //       toast.success(res.data.message);
-    //     })
-    //     .catch((err) => {
-    //       console.log("Errrr", err);
-    //       toast.error(err);
-    //     });
-    }
-    // else {
-    // toast.error("Please enter vjti email address");
-    // return;
-    // }
-    // return;
-    // }
-  };
+	const [insurances, setInsurances] = useState([]);
+	const fetchUser = useCallback(async () => {
+		try {
+			const user = await fetchCompanyByAddress(currentAccount);
 
-  return (
-    <div className={styles.mainContainer}>
-      <ToastContainer />
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        ariaHideApp={false}
-        contentLabel="Enter OTP"
-        style={{
-          content: {
-            top: "50%",
-            left: "50%",
-            right: "auto",
-            bottom: "auto",
-            marginRight: "-50%",
-            transform: "translate(-50%, -50%)",
-            position: "absolute",
-          },
-        }}
-      >
-        <div className={styles.modalContainer}>
-          <button className={styles.closeButton} onClick={closeModal}>
-            <CloseIcon />
-          </button>
-          <h2
-            className={styles.heading}
-            style={{
-              width: "100%",
-              textAlign: "center",
-            }}
-          >
-            Enter details
-          </h2>
+			const contractAddress = await fetchInsuranceAddress();
 
-          <form>
-            <div className={styles.inputContainer}>
-              <input
-                className={`${styles.modalInput}`}
-                style={{
-                  resize: "none",
-                }}
-                type="number"
-                placeholder="Enter Premium"
-                onChange={(e) => setPremium(e.target.value)}
-                value={premium}
-              />
-            </div>
-            <div className={styles.inputContainer}>
-              <input
-                className={`${styles.modalInput}`}
-                style={{
-                  resize: "none",
-                }}
-                type="number"
-                placeholder="Enter Payout"
-                onChange={(e) => setPayout(e.target.value)}
-                value={payout}
-              />
-            </div>
-            <div className={styles.inputContainer}>
-              <input
-                className={`${styles.modalInput}`}
-                style={{
-                  resize: "none",
-                }}
-                type="text"
-                placeholder="Enter duration"
-                onChange={(e) => setDuration(e.target.value)}
-                value={duration}
-              />
-            </div>
-            <button
-              className={`${styles.submitButton}`}
-              onClick={handleSubmit}
-            >
-              {isLoading ? (
-                <MoonLoader className={styles.loader} color="white" size={10} />
-              ) : (
-                "Submit"
-              )}
-            </button>
-          </form>
-        </div>
-      </Modal>{" "}
-      <div className={styles.secondContainer}>
-        <h1 className={styles.mainHeading}>Insurance Company</h1>
-        <div className={styles.viewFarms}>
-          <h2 className={styles.farmsHead}>View My Customers</h2>
-          <div className={styles.shadow}></div>
-          <div className={styles.farmsCards}>
-            <div className={styles.farm}>
-              <div className={styles.headFarm}>FileName</div>
-              <div className={styles.details}>
-                Location: <span className={styles.values}>Latur</span>
-              </div>
-              <div className={styles.details}>
-                LandArea: <span className={styles.values}>2 acre</span>
-              </div>
-              <div className={styles.details}>
-                CropType: <span className={styles.values}>Rabi</span>
-              </div>
-              <button className={styles.button}>Verify</button>
-            </div>
-            <div className={styles.farm}>
-              <div className={styles.headFarm}>FileName</div>
-              <div className={styles.details}>
-                Location: <span className={styles.values}>Bhusawal</span>
-              </div>
-              <div className={styles.details}>
-                LandArea: <span className={styles.values}>1 acre</span>
-              </div>
-              <div className={styles.details}>
-                CropType: <span className={styles.values}>Kharif</span>
-              </div>
-              <button className={styles.button}>Verify</button>
-            </div>
-          </div>
-        </div>
-        <div className={styles.viewInsurances}>
-          <div className={styles.insuranceHeadBox}>
-            <h2 className={styles.insuranceHead}>View My Insurances</h2>
-            <button
-                onClick={openModal}
-              className={styles.button2}
-            >
-              Add Insurance
-            </button>
-          </div>
-          <div className={styles.shadow}></div>
-          <div className={styles.insuranceCards}>
-            <div className={styles.insurance}>
-              {/* uint256 userId;
-        string location;
-        uint256 landArea;
-        string cropType;
-        string ipfsHash;
-        string fileName; */}
-              <div className={styles.headInsurance}>Insurance</div>
-              <div className={styles.details}>
-                Payout: <span className={styles.values}>1 year</span>
-              </div>
-              <div className={styles.details}>
-                Premium: <span className={styles.values}>1000</span>
-              </div>
-              <div className={styles.details}>
-                Duration: <span className={styles.values}>7th April</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+			const data = await fetchMyInsList(contractAddress);
+			console.log(data);
+			setInsurances(data);
+		} catch (err) {
+			navigate("/register");
+		}
+	});
+
+	useEffect(() => {
+		if (currentAccount) fetchUser();
+		connectUsingArcana();
+	}, [currentAccount]);
+
+	function closeModal() {
+		setModalIsOpen(false);
+	}
+
+	const openModal = async (e) => {
+		e.preventDefault();
+		setModalIsOpen(true);
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (duration === "" || premium === "" || payout === "") {
+			toast.error("Enter all details");
+			return;
+		} else {
+			toast.warn("Running! be patient!");
+			const contractAddress = await fetchInsuranceAddress();
+			await addNewInsuranceType(
+				contractAddress,
+				premium,
+				payout,
+				duration
+			);
+			toast.success("Added!");
+		}
+	};
+
+	return (
+		<div className={styles.mainContainer}>
+			<ToastContainer />
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				ariaHideApp={false}
+				contentLabel="Enter OTP"
+				style={{
+					content: {
+						top: "50%",
+						left: "50%",
+						right: "auto",
+						bottom: "auto",
+						marginRight: "-50%",
+						transform: "translate(-50%, -50%)",
+						position: "absolute",
+					},
+				}}
+			>
+				<div className={styles.modalContainer}>
+					<button className={styles.closeButton} onClick={closeModal}>
+						<CloseIcon />
+					</button>
+					<h2
+						className={styles.heading}
+						style={{
+							width: "100%",
+							textAlign: "center",
+						}}
+					>
+						Enter details
+					</h2>
+
+					<form>
+						<div className={styles.inputContainer}>
+							<input
+								className={`${styles.modalInput}`}
+								style={{
+									resize: "none",
+								}}
+								type="number"
+								placeholder="Enter Premium"
+								onChange={(e) => setPremium(e.target.value)}
+								value={premium}
+							/>
+						</div>
+						<div className={styles.inputContainer}>
+							<input
+								className={`${styles.modalInput}`}
+								style={{
+									resize: "none",
+								}}
+								type="number"
+								placeholder="Enter Payout"
+								onChange={(e) => setPayout(e.target.value)}
+								value={payout}
+							/>
+						</div>
+						<div className={styles.inputContainer}>
+							<input
+								className={`${styles.modalInput}`}
+								style={{
+									resize: "none",
+								}}
+								type="text"
+								placeholder="Enter duration"
+								onChange={(e) => setDuration(e.target.value)}
+								value={duration}
+							/>
+						</div>
+						<button
+							className={`${styles.submitButton}`}
+							onClick={handleSubmit}
+						>
+							{isLoading ? (
+								<MoonLoader
+									className={styles.loader}
+									color="white"
+									size={10}
+								/>
+							) : (
+								"Submit"
+							)}
+						</button>
+					</form>
+				</div>
+			</Modal>{" "}
+			<div className={styles.secondContainer}>
+				<h1 className={styles.mainHeading}>Insurance Company</h1>
+				<div className={styles.viewFarms}>
+					<h2 className={styles.farmsHead}>View My Customers</h2>
+					<div className={styles.shadow}></div>
+					<div className={styles.farmsCards}>
+						<div className={styles.farm}>
+							<div className={styles.headFarm}>FileName</div>
+							<div className={styles.details}>
+								Location:{" "}
+								<span className={styles.values}>Latur</span>
+							</div>
+							<div className={styles.details}>
+								LandArea:{" "}
+								<span className={styles.values}>2 acre</span>
+							</div>
+							<div className={styles.details}>
+								CropType:{" "}
+								<span className={styles.values}>Rabi</span>
+							</div>
+							<button className={styles.button}>Verify</button>
+						</div>
+						<div className={styles.farm}>
+							<div className={styles.headFarm}>FileName</div>
+							<div className={styles.details}>
+								Location:{" "}
+								<span className={styles.values}>Bhusawal</span>
+							</div>
+							<div className={styles.details}>
+								LandArea:{" "}
+								<span className={styles.values}>1 acre</span>
+							</div>
+							<div className={styles.details}>
+								CropType:{" "}
+								<span className={styles.values}>Kharif</span>
+							</div>
+							<button className={styles.button}>Verify</button>
+						</div>
+					</div>
+				</div>
+				<div className={styles.viewInsurances}>
+					<div className={styles.insuranceHeadBox}>
+						<h2 className={styles.insuranceHead}>
+							View My Insurances
+						</h2>
+						<button onClick={openModal} className={styles.button2}>
+							Add Insurance
+						</button>
+					</div>
+					<div className={styles.shadow}></div>
+					<div className={styles.insuranceCards}>
+						{insurances.map((val, index) => {
+							return (
+								<div className={styles.insurance}>
+									<div className={styles.headInsurance}>
+										Insurance
+									</div>
+									<div className={styles.details}>
+										Payout:{" "}
+										<span className={styles.values}>
+											{val.payout.toString()}
+										</span>
+									</div>
+									<div className={styles.details}>
+										Premium:{" "}
+										<span className={styles.values}>
+											{val.premium.toString()}
+										</span>
+									</div>
+									<div className={styles.details}>
+										Duration:{" "}
+										<span className={styles.values}>
+											{val.duration.toString()} months
+										</span>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default CompanyDashboard;
