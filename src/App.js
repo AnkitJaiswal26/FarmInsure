50163d48c53d687b1eb9e96ed2d761c3e781173c
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-04-01 13:29:51 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import RegisterFarmer from "./pages/RegisterFarmer/RegisterFarmer";
+import MainPage from "./pages/MainPage/MainPage";
+import Register from "./pages/Register/Register";
+import RegisterCompany from "./pages/RegisterCompany/RegisterCompany";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: (
+				<>
+					<MainPage />
+				</>
+			),
+		},
+		{
+			path: "/register",
+			element: (
+				<>
+					<Register />
+				</>
+			),
+		},
+		{
+			path: "/registerfarmer",
+			element: (
+				<>
+					{/* <Navbar /> */}
+					<RegisterFarmer />
+				</>
+			),
+		},
+		{
+			path: "registercompany",
+			element: (
+				<>
+					<RegisterCompany />
+				</>
+			),
+		},
+	]);
+
+	return (
+		<>
+			<RouterProvider router={router}></RouterProvider>
+		</>
+	);
+};
 
 export default App;
